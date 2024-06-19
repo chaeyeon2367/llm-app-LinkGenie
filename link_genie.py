42df56891bdfe219ce12fb5433477ae01e8fe16c
Shim chaeyeon <chaeyeon.shim@etu.univ-cotedazur.fr>
2024-06-20 00:06:45 +0200

    Custom Search Agent Testing

--- link_genie.py
+++ link_genie.py
@@ -4,21 +4,17 @@ from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-load_dotenv()
-
-if __name__ == "__main__":
-    load_dotenv()
-
-    print("Hello LangChain!")
-
-    # linkedin_profile_url = linkedin_lookup_agent(name="Chaeyeon Shim")
+def link_genie_with(name: str)-> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
-    given the information {information} about a person I want you to create :
-    1. A short summary
-    2. two interesting facts about them
-    """
+        given the information {information} about a person I want you to create :
+        1. A short summary
+        2. two interesting facts about them
+        """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
@@ -28,11 +24,17 @@ if __name__ == "__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/chaeyeonshim0930/", mock=True
-    )
-
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Link Genie Enter")
+    link_genie_with(name="Chaeyeon Shim Data scientist")
+
+
+
 
