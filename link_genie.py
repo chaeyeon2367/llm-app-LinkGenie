184ed8b3764a80d04e1cd24e3eecfd548bf60d27
chaeyeon2367 <chaeyeon2367@gmail.com>
2024-06-20 01:02:27 +0200

    add summary and profile_pic_url

--- link_genie.py
+++ link_genie.py
@@ -1,13 +1,14 @@
+from typing import Tuple
+
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
-def link_genie_with(name: str)-> str:
+def link_genie_with(name: str)-> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
@@ -27,12 +28,11 @@ def link_genie_with(name: str)-> str:
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    #chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-
     chain = summary_prompt_template | llm | summary_parser
-    res = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    res:Summary = chain.invoke(input={"information": linkedin_data})
+
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
