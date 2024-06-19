2a2c442ebb7f806fad8a2a361f0b1c9719fd2073
chaeyeon2367 <chaeyeon2367@gmail.com>
2024-06-20 00:51:35 +0200

    add output parser

--- link_genie.py
+++ link_genie.py
@@ -3,6 +3,7 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
@@ -14,16 +15,21 @@ def link_genie_with(name: str)-> str:
         given the information {information} about a person I want you to create :
         1. A short summary
         2. two interesting facts about them
+        
+        Use information from Linkedin
+        \n{format_instructions}
         """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions":summary_parser.get_format_instructions()},
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    #chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
+    chain = summary_prompt_template | llm | summary_parser
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
