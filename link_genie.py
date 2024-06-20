5d6bffe05d5eb712bb589d8906bdaad485c04c79
chaeyeon2367 <chaeyeon2367@gmail.com>
2024-06-20 02:36:45 +0200

    updated LangSmith

--- link_genie.py
+++ link_genie.py
@@ -11,7 +11,7 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 def link_genie_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
         given the information {information} about a person I want you to create :
